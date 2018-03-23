b056d718850b94ef909b0ea01c3fcd0a80d8f34b
Ezeugo Aguta <zugo01@yahoo.com>
2018-03-23 14:17:42 +0000

    Add files via upload

---
+++ www/shared/zugo.js
@@ -0,0 +1,113 @@
+function CopyValues(f) {
+    f.chkPLEF1.checked = f.chkPLEF.checked;
+    f.PLEFMin1.value = f.PLEFMin.value;
+    f.PLEFML1.value = f.PLEFML.value;
+    f.PLEFMax1.value = f.PLEFMax.value;
+    f.PLEFConf1.value = f.PLEFConf.value;
+    f.chkTEF1.checked = f.chkTEF.checked;
+    f.TEFMin1.value = f.TEFMin.value;
+    f.TEFML1.value = f.TEFML.value;
+    f.TEFMax1.value = f.TEFMax.value;
+    f.TEFConf1.value = f.TEFConf.value;
+    f.chkCF1.checked = f.chkCF.checked;
+    f.CFMin1.value = f.CFMin.value;
+    f.CFML1.value = f.CFML.value;
+    f.CFMax1.value = f.CFMax.value;
+    f.CFConf1.value = f.CFConf.value;
+    f.chkPOA1.checked = f.chkPOA.checked;
+    f.POAMin1.value = f.POAMin.value;
+    f.POAML1.value = f.POAML.value;
+    f.POAMax1.value = f.POAMax.value;
+    f.POAConf1.value = f.POAConf.value;
+    f.chkVuln1.checked = f.chkVuln.checked;
+    f.VulnMin1.value = f.VulnMin.value;
+    f.VulnML1.value = f.VulnML.value;
+    f.VulnMax1.value = f.VulnMax.value;
+    f.VulnConf1.value = f.VulnConf.value;
+    f.chkTC1.checked = f.chkTC.checked;
+    f.TCMin1.value = f.TCMin.value;
+    f.TCML1.value = f.TCML.value;
+    f.TCMax1.value = f.TCMax.value;
+    f.TCConf1.value = f.TCConf.value;
+    f.chkRS1.checked = f.chkRS.checked;
+    f.RSMin1.value = f.RSMin.value;
+    f.RSML1.value = f.RSML.value;
+    f.RSMax1.value = f.RSMax.value;
+    f.RSConf1.value = f.RSConf.value;
+    f.PrimaryProductivityMin1.value = f.PrimaryProductivityMin.value;
+    f.PrimaryProductivityML1.value = f.PrimaryProductivityML.value;
+    f.PrimaryProductivityMax1.value = f.PrimaryProductivityMax.value;
+    f.PrimaryProductivityConf1.value = f.PrimaryProductivityConf.value;
+    f.PrimaryResponseMin1.value = f.PrimaryResponseMin.value;
+    f.PrimaryResponseML1.value = f.PrimaryResponseML.value;
+    f.PrimaryResponseMax1.value = f.PrimaryResponseMax.value;
+    f.PrimaryResponseConf1.value = f.PrimaryResponseConf.value;
+    f.PrimaryReplacementMin1.value = f.PrimaryReplacementMin.value;
+    f.PrimaryReplacementML1.value = f.PrimaryReplacementML.value;
+    f.PrimaryReplacementMax1.value = f.PrimaryReplacementMax.value;
+    f.PrimaryReplacementConf1.value = f.PrimaryReplacementConf.value;
+    f.PrimaryFandJMin1.value = f.PrimaryFandJMin.value;
+    f.PrimaryFandJML1.value = f.PrimaryFandJML.value;
+    f.PrimaryFandJMax1.value = f.PrimaryFandJMax.value;
+    f.PrimaryFandJConf1.value = f.PrimaryFandJConf.value;
+    f.PrimaryCompAdvMin1.value = f.PrimaryCompAdvMin.value;
+    f.PrimaryCompAdvML1.value = f.PrimaryCompAdvML.value;
+    f.PrimaryCompAdvMax1.value = f.PrimaryCompAdvMax.value;
+    f.PrimaryCompAdvConf1.value = f.PrimaryCompAdvConf.value;
+    f.PrimaryReputationMin1.value = f.PrimaryReputationMin.value;
+    f.PrimaryReputationML1.value = f.PrimaryReputationML.value;
+    f.PrimaryReputationMax1.value = f.PrimaryReputationMax.value;
+    f.PrimaryReputationConf1.value = f.PrimaryReputationConf.value;
+    f.SLEFMin1.value = f.SLEFMin.value;
+    f.SLEFML1.value = f.SLEFML.value;
+    f.SLEFMax1.value = f.SLEFMax.value;
+    f.SLEFConf1.value = f.SLEFConf.value;
+	f.SecondaryProductivityMin1.value = f.SecondaryProductivityMin.value;
+    f.SecondaryProductivityML1.value = f.SecondaryProductivityML.value;
+    f.SecondaryProductivityMax1.value = f.SecondaryProductivityMax.value;
+    f.SecondaryProductivityConf1.value = f.SecondaryProductivityConf.value;
+    f.SecondaryResponseMin1.value = f.SecondaryResponseMin.value;
+    f.SecondaryResponseML1.value = f.SecondaryResponseML.value;
+    f.SecondaryResponseMax1.value = f.SecondaryResponseMax.value;
+    f.SecondaryResponseConf1.value = f.SecondaryResponseConf.value;
+    f.SecondaryReplacementMin1.value = f.SecondaryReplacementMin.value;
+    f.SecondaryReplacementML1.value = f.SecondaryReplacementML.value;
+    f.SecondaryReplacementMax1.value = f.SecondaryReplacementMax.value;
+    f.SecondaryReplacementConf1.value = f.SecondaryReplacementConf.value;
+    f.SecondaryFandJMin1.value = f.SecondaryFandJMin.value;
+    f.SecondaryFandJML1.value = f.SecondaryFandJML.value;
+    f.SecondaryFandJMax1.value = f.SecondaryFandJMax.value;
+    f.SecondaryFandJConf1.value = f.SecondaryFandJConf.value;
+    f.SecondaryCompAdvMin1.value = f.SecondaryCompAdvMin.value;
+    f.SecondaryCompAdvML1.value = f.SecondaryCompAdvML.value;
+    f.SecondaryCompAdvMax1.value = f.SecondaryCompAdvMax.value;
+    f.SecondaryCompAdvConf1.value = f.SecondaryCompAdvConf.value;
+    f.SecondaryReputationMin1.value = f.SecondaryReputationMin.value;
+    f.SecondaryReputationML1.value = f.SecondaryReputationML.value;
+    f.SecondaryReputationMax1.value = f.SecondaryReputationMax.value;
+    f.SecondaryReputationConf1.value = f.SecondaryReputationConf.value;
+    f.ServiceDowntimeMin1.value = f.ServiceDowntimeMin.value;
+    f.ServiceDowntimeML1.value = f.ServiceDowntimeML.value;
+    f.ServiceDowntimeMax1.value = f.ServiceDowntimeMax.value;
+    f.ServiceDowntimeConf1.value = f.ServiceDowntimeConf.value;
+    f.DataLossMin1.value = f.DataLossMin.value;
+    f.DataLossML1.value = f.DataLossML.value;
+    f.DataLossMax1.value = f.DataLossMax.value;
+    f.DataLossConf1.value = f.DataLossConf.value;
+
+
+    var elems = document.body.getElementsByTagName("*");
+
+    $(elems).change();
+    //$(f.chkPLEF1).add(f.PLEFMin1).add(f.PLEFML1).add(f.PLEFMax1).change();
+}
+
+function scenario1(){
+    $('#outputTabs a[href="#tab-5222-1"]').tab('show');
+    $('#inputTabs a[href="#tab-5222-11"]').tab('show');
+}
+
+function scenario2(){
+    $('#outputTabs a[href="#tab-5222-2"]').tab('show');
+    $('#inputTabs a[href="#tab-5222-22"]').tab('show');
+}
